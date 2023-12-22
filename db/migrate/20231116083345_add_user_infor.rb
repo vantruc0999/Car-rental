9b7be6d0804ae802969135c5cda79a66346ef43d
truc0409 <vantruc0409@gmail.com>
2023-12-22 15:29:34 +0700

    change db config

--- db/migrate/20231116083345_add_user_infor.rb
+++
@@ -1,9 +0,0 @@
-class AddUserInfor < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :birth_day, :date
-    add_column :phone_number, :string
-    add_column :province, :string
-    add_column :city, :string
-  end
-end
